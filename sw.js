c2eaed543aac0635564258a25c330b11c0ca91d3
Eduardo Pellissari Moreira <Eduardo@MacBook-Pro-de-Eduardo.local>
2026-06-16 15:07:01 -0300

    Force PWA cache refresh

--- sw.js
+++ sw.js
@@ -14,7 +14,17 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
